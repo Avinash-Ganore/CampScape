cdb394c301456c03dd4c2ec1ec93a32962e2cce0
Avinash Ganore <avinashganore@gmail.com>
2025-05-01 19:31:38 +0530

    Complete create and read operation

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import methodOverride from "method-override";
 import path from "path";
-import campground from "./models/campground.js";
+import Campground from "./models/campground.js";
 
 mongoose.connect("mongodb://localhost:27017/yelpCamp",{
         useNewUrlParser : true,
         useUnifiedTopology : true
 })
-.then(() => {
-    console.log("Database connected!!");
-})
+.then(() => console.log("Database connected!!"))
 .catch(err => {
     console.log("Error!!");
     console.log(err);
@@ -19,17 +17,37 @@ mongoose.connect("mongodb://localhost:27017/yelpCamp",{
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended: true}));
 app.set("views", path.join(import.meta.dirname , "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {})
-
-app.get("/make", async (req, res) => {
-    const camp = new campground({title : "Avinash",location : "India"})
-    await camp.save();
-    res.send(camp)
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", {campgrounds});
 })
 
-app.listen(3000, () => {
-    console.log(`Serving on 3000`);
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/create");
+})
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground= await Campground.findById(req.params.id);
+    res.render("campgrounds/show",{campground})
+})
+
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+app.post("/campgrounds", async (req, res) => {
+    req.body.campground.title = capitalize(req.body.campground.title);
+    req.body.city = capitalize(req.body.city);
+    req.body.state = capitalize(req.body.state);   
+    req.body.campground.location = `${req.body.city}, ${req.body.state}`;
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp._id}`);
+})
+
+
+app.listen(port, () => {
+    console.log(`Serving on http://localhost:${port}`);
 })
